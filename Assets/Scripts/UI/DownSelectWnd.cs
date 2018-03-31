a1999dad693686ad4a4a07103f6baaf5a3929e61
FlyLing <963452757@qq.com>
2018-03-31 18:20:13 +0800

    漏传修改，添加下方选框

---
+++ Assets/Scripts/UI/DownSelectWnd.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DownSelectWndType
+{
+    terrainTexture,
+}
+public class DownSelectWnd : MonoBehaviour {
+
+
+    public GameObject selectItem = null;
+
+    Transform root = null;
+    List<DownSelectWndItem> itemClass = null;
+
+    private void Start()
+    {
+        root = transform.Find("ScrollView/Viewport/Content");
+    }
+
+    public void Init(List<BaseInfo> infoList, DownSelectWndType type)
+    {
+        for(int i=0;i<infoList.Count;i++)
+        {
+            GameObject item = GameObject.Instantiate(selectItem);
+            item.transform.parent = root;
+            ItemAddComponent(item, DownSelectWndType.terrainTexture);
+        }
+    } 
+
+    void ItemAddComponent(GameObject item,DownSelectWndType type)
+    {
+        switch(type)
+        {
+            case DownSelectWndType.terrainTexture:
+                item.AddComponent<TerrainSelectItem>();
+                break;
+        }
+    }
+
+    public void SetInfo(List<BaseInfo> infoList,List<DownSelectWndItem> itemList)
+    {
+        //for(int i)
+    }
+}
---
+++ Assets/Scripts/UI/TerrainSelectItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSelectItem : DownSelectWndItem {
+
+    TerrainTypes terrainType = 0;
+
+
+    public override void OnItemClick(bool isOn)
+    {
+        if(isOn)
+        {
+            HexMetrics.instance.editorTerrainType = terrainType;
+        }
+    }
+
+    public override void Init()
+    {
+        base.Init();
+    }
+
+    public override void SetInfo(BaseInfo baseInfo)
+    {
+        TerrainTextureInfo info = (TerrainTextureInfo)baseInfo;
+        base.SetInfo(info);
+        terrainType = info.terrainType;
+    }
+
+
+}
